3229e157f76e591f5bb9cf46797eb5c525e01a7b
danielm19 <danielm19@csu.fullerton.edu>
2022-10-16 18:33:59 -0700

    added node class

--- pacman.py
+++ pacman.py
@@ -2,15 +2,22 @@ import pygame as pg
 from pygame.locals import *
 from settings import Settings
 from vector import Vector
+import gamefunctions as gf
 
-class Pacman(object):
-    def __init__(self, game):
+class Pacman:
+    def __init__(self, game, node):
         self.settings = Settings()
         self.screen = game.screen
         self.position = Vector(350, 600)
         self.vel = Vector()
         self.radius = 15
         self.color = self.settings.yellow
+        self.node = node
+        self.set_pos()
+        
+    def set_pos(self):
+        self.position = self.node.pos.copy()
+        
         
     def update(self):
         self.position += self.vel
