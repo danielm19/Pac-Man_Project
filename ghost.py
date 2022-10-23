859707bc4bf62306c6c52d2ad7414f1d972cee0e
Stephanie Becerra <stephanie.becerra.2018@gmail.com>
2022-10-22 18:31:08 -0700

    reverted ghost.py
    went back to the old code that was in ghost.py since we currently don't have a entity.py

--- ghost.py
+++ ghost.py
@@ -4,16 +4,12 @@ from vector import Vector
 from settings import Settings
 from character import Character
 
-class Ghost(Entity):
-    def __init__(self, node, pacman=None, blinky=None):
-        Entity.__init__(self, node)
-        self.name = GHOST
-        self.points = 200
-        self.goal = Vector()
-        self.directionMethod = self.goalDirection
-        self.pacman = pacman
-        self.mode = ModeController(self)
-        self.blinky = blinky
-        self.homeNode = node
+class Ghost(Character):
+    def __init__(self, game, node):
+        Character.__init__(self, game, node)
+        self.points = 250
+
+# need to create a function to calculate the movement
+# need the update function and draw
 
     
